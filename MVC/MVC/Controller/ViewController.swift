a12bffc1177eaeb978c1f1603d3fc98c5ef5a595
Fábio França <fabiotibete@gmail.com>
2020-08-02 17:14:41 -0300

    create model

--- MVC/MVC/Controller/ViewController.swift
+++ MVC/MVC/Controller/ViewController.swift
@@ -14,12 +14,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var detailLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    var movie = Movie(title: "Harry Potter", description: "Um bruxo na escola de magia", detail: "Harry descobre que é um bruxo e começa sua aventura em busca de respostas e descobertas.")
 
     @IBAction func updateBtnWasPressed(_ sender: Any) {
+        titleLabel.text = movie.title
+        descriptionLabel.text = movie.description
+        detailLabel.text = movie.detail
     }
     
 }
---
+++ MVC/MVC/Model/Movie.swift
@@ -0,0 +1,15 @@
+//
+//  Movie.swift
+//  MVC
+//
+//  Created by Fábio França on 02/08/20.
+//  Copyright © 2020 music.com.chama. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    var title: String
+    var description: String
+    var detail: String
+}
